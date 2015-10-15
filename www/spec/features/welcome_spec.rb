70b553e47fb89c84df64beb5bf44f5f2dc14e9eb
ren <code@apotheon.net>
2015-10-15 14:54:54 +0000

    really remove www

--- www/spec/features/welcome_spec.rb
+++
@@ -1,8 +0,0 @@
-require 'rails_helper'
-
-feature 'Welcome Page' do
-  scenario 'a visitor opens the homepage' do
-    visit root_path
-    expect(page).to have_content 'Welcome to Fossil Record.'
-  end
-end
