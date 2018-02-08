c811fdb5ccc7d05c334ba9d71622874bf46afbbc
ren <code@apotheon.net>
2018-02-08 04:42:50 +0000

    rearrange q.c

--- src/q.c
+++ src/q.c
@@ -11,28 +11,30 @@ typedef enum { false, true } bool;
 char *dirname = ".quebert";
 char *qname = "queue.txt";
 
+bool cd();
+bool cd_qdir();
 bool exists(char *fname);
 bool get_line(char *line, FILE *qfile);
+bool help();
 bool match_cmd(char *cmd, char *cmdtarget);
 bool match_help(char *cmd);
 bool match_rot(char *cmd);
+bool missing(char *fname);
+bool newdir(char *dir);
 bool qexists();
+bool usage(char *self);
 
-int add_item(char *input);
-int cd();
-int cd_qdir();
-int help();
-int list_all();
-int not_implemented(char *cmd);
-int print_error_empty();
-int print_error_exists(char *dir, char *q);
-int print_error_open();
-int print_error_qfile_missing();
-int print_numbered_file_listing(FILE *qfile);
-int show_head();
-int start_queuer();
-int try_help();
-int usage(char *self);
+void add_item(char *input);
+void list_all();
+void not_implemented(char *cmd);
+void print_error_empty();
+void print_error_exists(char *dir, char *q);
+void print_error_open();
+void print_error_qfile_missing();
+void print_numbered_file_listing(FILE *qfile);
+void show_head();
+void start_queuer();
+void try_help();
 
 int main(int argc, char **argv) {
 	char *program = *(argv);
@@ -76,22 +78,37 @@ int main(int argc, char **argv) {
  /* OTHER FUNCTIONS */
 /*******************/
 
+bool cd(char *dir) {
+	if (chdir(dir) == 0) return true;
+	else return false;
+}
+
+bool cd_qdir() {
+	int success = false;
+	char *home = getenv("HOME");
+
+	if (cd(home)) {
+		if (missing(dirname)) newdir(dirname);
+		success = cd(dirname);
+	}
+
+	return success;
+}
+
 bool exists(char *fname) {
 	struct stat st = {0};
 
-	if (!stat(fname, &st)) return true;
+	if (stat(fname, &st) == 0) return true;
 	else return false;
 }
 
 bool get_line(char *line, FILE *qfile) {
-	if (fgets(line, LINESIZE - 1, qfile)) return true;
+	if (fgets(line, LINESIZE - 1, qfile) > 0) return true;
 	else return false;
 }
 
 bool match_cmd(char *cmd, char *cmdtarget) {
-	int l = strnlen(cmd, LINESIZE);
-
-	if (!strncmp(cmd, cmdtarget, l)) return true;
+	if (strncmp(cmd, cmdtarget, strnlen(cmd, LINESIZE)) == 0) return true;
 	else return false;
 }
 
@@ -106,140 +123,25 @@ bool match_rot(char *cmd) {
 	return (match_cmd(cmd, "rot") || match_cmd(cmd, "rotate"));
 }
 
+bool missing(char *fname) {
+	return ! exists(fname);
+}
+
+bool newdir(char *dir) {
+	if (mkdir(dir, S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH) == 0) return true;
+	else return false;
+}
+
 bool qexists() {
 	return (cd_qdir() && exists(qname));
 }
 
-int usage(char *self) {
-	printf("%s <command> [argument]\n", self);
-	return 1;
+bool usage(char *self) {
+	if (printf("%s <command> [argument]\n", self)) return true;
+	else return false;
 }
 
-int try_help(char *self) {
-	printf("Try \"%s help\" for more info.\n", self);
-	return 1;
-}
-
-int newdir(char *dir) {
-	return mkdir(dir, S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH);
-}
-
-int not_implemented(char *cmd) {
-	printf("Command \"%s\" was not yet implemented.\n", cmd);
-	return 0;
-}
-
-int cd(char *dir) {
-	return !chdir(dir);
-}
-
-int cd_qdir() {
-	int success = 0;
-	char *home = getenv("HOME");
-
-	if (cd(home)) {
-		if (!exists(dirname)) newdir(dirname);
-		success = cd(dirname);
-	}
-
-	return success;
-}
-
-int print_error_empty() {
-	perror("Error reading from queuefile (it may be empty)");
-	return 0;
-}
-
-int print_error_exists(char *dir, char *q) {
-	char *home = getenv("HOME");
-	printf("The file \"%s/%s/%s\" already exists.\n", home, dir, q);
-	return 0;
-}
-
-int print_error_open() {
-	perror("Error opening queuefile.");
-	return 0;
-}
-
-int print_error_qfile_missing() {
-	puts("No queuefile found.  Create one with `q create-fresh-queue`.");
-	return 0;
-}
-
-int add_item(char *input) {
-	if (qexists()) {
-		FILE *qfile = fopen(qname, "a");
-
-		if (! qfile) print_error_open();
-		else fprintf(qfile, "%s\n", input);
-
-		fclose(qfile);
-	} else {
-		print_error_qfile_missing();
-	}
-
-	return 0;
-}
-
-int list_all() {
-	if (qexists()) {
-		FILE *qfile = fopen(qname, "r");
-
-		if (! qfile) print_error_open();
-		else print_numbered_file_listing(qfile);
-
-		fclose(qfile);
-	} else {
-		print_error_qfile_missing();
-	}
-
-	return 0;
-}
-
-int show_head() {
-	if (cd_qdir() && exists(qname)) {
-		FILE *qfile = fopen(qname, "r");
-		char *line = (char*) malloc(LINESIZE);
-
-		if (! qfile) print_error_open();
-		else if (get_line(line, qfile)) printf("%s", line);
-		else print_error_empty();
-
-		fclose(qfile);
-	} else {
-		print_error_qfile_missing();
-	}
-
-	return 0;
-}
-
-int print_numbered_file_listing(FILE *qfile) {
-	int n = 0;
-	char *line = (char*) malloc(LINESIZE);
-	bool next = false;
-
-	if ((next = get_line(line, qfile))) {
-		while (next) {
-			printf("%4d %s", ++n, line);
-			next = get_line(line, qfile);
-		}
-	} else {
-		print_error_empty();
-	}
-
-	return 0;
-}
-
-int start_queuer() {
-	cd_qdir();
-
-	if (exists(qname)) print_error_exists(dirname, qname);
-	else open(qname, O_CREAT, 0600);
-
-	return 0;
-}
-
-int help() {
+bool help() {
 	puts("");
 	puts("This queuer is a simple queue manager.  You can add things to the");
 	puts("end of the queue.  You can pop things off the beginning.  You can");
@@ -271,5 +173,93 @@ int help() {
 	puts("    create-fresh-queue      set up queuer for use");
 	puts("");
 
-	return 1;
+	return true;
+}
+
+void add_item(char *input) {
+	if (qexists()) {
+		FILE *qfile = fopen(qname, "a");
+
+		if (! qfile) print_error_open();
+		else fprintf(qfile, "%s\n", input);
+
+		fclose(qfile);
+	} else {
+		print_error_qfile_missing();
+	}
+}
+
+void list_all() {
+	if (qexists()) {
+		FILE *qfile = fopen(qname, "r");
+
+		if (! qfile) print_error_open();
+		else print_numbered_file_listing(qfile);
+
+		fclose(qfile);
+	} else {
+		print_error_qfile_missing();
+	}
+}
+
+void show_head() {
+	if (cd_qdir() && exists(qname)) {
+		FILE *qfile = fopen(qname, "r");
+		char *line = (char*) malloc(LINESIZE);
+
+		if (! qfile) print_error_open();
+		else if (get_line(line, qfile)) printf("%s", line);
+		else print_error_empty();
+
+		fclose(qfile);
+	} else {
+		print_error_qfile_missing();
+	}
+}
+
+void not_implemented(char *cmd) {
+	printf("Command \"%s\" was not yet implemented.\n", cmd);
+}
+
+void print_error_empty() {
+	perror("Error reading from queuefile (it may be empty)");
+}
+
+void print_error_exists(char *dir, char *q) {
+	char *home = getenv("HOME");
+	printf("The file \"%s/%s/%s\" already exists.\n", home, dir, q);
+}
+
+void print_error_open() {
+	perror("Error opening queuefile.");
+}
+
+void print_error_qfile_missing() {
+	puts("No queuefile found.  Create one with `q create-fresh-queue`.");
+}
+
+void print_numbered_file_listing(FILE *qfile) {
+	int n = 0;
+	char *line = (char*) malloc(LINESIZE);
+	bool next = false;
+
+	if ((next = get_line(line, qfile))) {
+		while (next) {
+			printf("%4d %s", ++n, line);
+			next = get_line(line, qfile);
+		}
+	} else {
+		print_error_empty();
+	}
+}
+
+void start_queuer() {
+	cd_qdir();
+
+	if (exists(qname)) print_error_exists(dirname, qname);
+	else open(qname, O_CREAT, 0600);
+}
+
+void try_help(char *self) {
+	printf("Try \"%s help\" for more info.\n", self);
 }
