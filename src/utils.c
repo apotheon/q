2369967fb616b498b75ff012be786812a9213da2
ren <ren@noemail.net>
2018-02-11 16:49:13 +0000

    stop repeating myself so much with pointer array initializations
    FossilOrigin-Name: a47ea755426cbd765abedfe613438146484321eb

--- src/utils.c
+++ src/utils.c
@@ -40,3 +40,15 @@ bool newdir(char *dir) {
 bool qexists() {
 	return (cd_qdir() && exists(QNAME));
 }
+
+/* pain is short for "pointer array initializer" */
+bool pain(char *parray, size_t parray_size) {
+	if (parray == NULL) {
+		free(parray);
+		perror("Failed to initialize an array pointer.\n");
+		exit(EXIT_FAILURE);
+	} else {
+		memzero(parray, parray_size);
+		return true;
+	}
+}
