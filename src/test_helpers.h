2f2e0a18ee47294c876f9fd32bb10086a14f0b48
ren <code@apotheon.net>
2018-03-06 22:49:04 +0000

    add create_dir(), create_qdir(), remove_dir(), and remove_qdir() test helpers

---
+++ src/test_helpers.h
@@ -0,0 +1,12 @@
+#ifndef TEST_HELPERS_H_
+#define TEST_HELPERS_H_
+
+#include "globals.h"
+#include "utils.h"
+
+#endif
+
+void create_dir(char *dirname);
+void create_qdir();
+void remove_dir(char *dirname);
+void remove_qdir();
