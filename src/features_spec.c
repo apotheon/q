d760585c1c57feadc673e53682db3b01f987402d
ren <code@apotheon.net>
2018-03-09 17:03:24 +0000

    reorganize and clean up features_spec.c; add add_item() test stub

--- src/features_spec.c
+++ src/features_spec.c
@@ -5,56 +5,45 @@
 #include "bdd-for-c.h"
 
 spec("Features") {
-	describe("del_item()") {
-		context("with queuefile containing todo items") {
-			before_each() {
-				prep_testq();
-			}
+	context("with queuefile containing todo items") {
+		FILE *qfile;
 
-			after_each() {
-				cleanup_testq();
-			}
+		char *line = calloc(LINESIZE, sizeof(*line));
+		check_alloc(line);
 
+		before_each() {
+			prep_testq();
+		}
+
+		after_each() {
+			cleanup_testq();
+			fclose(qfile);
+			cfree(line, LINESIZE);
+		}
+
+		describe("add_item()") {
+			it("adds a line at the end of the queuefile") {
+			}
+		}
+
+		describe("del_item()") {
 			it("deletes the first line in the queuefile") {
-				FILE *qfile = fopen(QNAME, "r");
-
-				char *line = calloc(LINESIZE, sizeof(*line));
-				check_alloc(line);
-
-				char *cwd = calloc(PATH_MAX, sizeof(*cwd));
-				check_alloc(cwd);
-				getcwd(cwd, PATH_MAX);
-
-				if (! qfile) {
-					exit(EXIT_FAILURE);
-				} else if (! get_line(line, qfile)) {
-					exit(EXIT_FAILURE);
-				}
-
-				fclose(qfile);
-
 				char *deleted = del_item();
-
 				qfile = fopen(QNAME, "r");
 
 				if (! qfile) {
 					fclose(qfile);
-					cfree(cwd, PATH_MAX);
 					clearfree(LINESIZE, 2, deleted, line);
 					exit(EXIT_FAILURE);
 				} else if (get_line(line, qfile)) {
 					check(strncmp(deleted, "FIRST LINE\n", LINESIZE) == 0);
 					check(strncmp(line, "SECOND LINE\n", LINESIZE) == 0);
 				} else {
-					puts(cwd);
-					cfree(cwd, PATH_MAX);
 					clearfree(LINESIZE, 2, deleted, line);
 					exit(EXIT_FAILURE);
 				}
 
-				fclose(qfile);
-				cfree(cwd, PATH_MAX);
-				clearfree(LINESIZE, 2, deleted, line);
+				cfree(deleted, LINESIZE);
 			}
 		}
 	}
