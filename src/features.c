d04c52095b8a33cdd08734ebb2620cc29e8614e1
ren <ren@noemail.net>
2018-03-12 17:33:31 +0000

    test add_item() feature and move it to features.c
    FossilOrigin-Name: 41cf9f819b370dd306e84c0506e9c84699f81a4f

--- src/features.c
+++ src/features.c
@@ -1,5 +1,18 @@
 #include "features.h"
 
+void add_item(char *input, char *self) {
+	if (qexists()) {
+		FILE *qfile = fopen(QNAME, "a");
+
+		if (! qfile) print_error_open();
+		else fprintf(qfile, "%s\n", input);
+
+		fclose(qfile);
+	} else {
+		print_error_qfile_missing(self);
+	}
+}
+
 char *del_item() {
 	return del_line(QNAME);
 }
--- src/features_spec.c
+++ src/features_spec.c
@@ -11,6 +11,9 @@ spec("Features") {
 		char *line = calloc(LINESIZE, sizeof(*line));
 		check_alloc(line);
 
+		char *line1 = "FIRST LINE\n";
+		char *line2 = "SECOND LINE\n";
+
 		before_each() {
 			prep_testq();
 		}
@@ -22,24 +25,36 @@ spec("Features") {
 		}
 
 		describe("add_item()") {
+			char *line3 = "THIRD LINE\n";
+			size_t s_len = strnlen(line1, LINESIZE) + strnlen(line2, LINESIZE);
+
 			it("adds a line at the end of the queuefile") {
+				add_item(line3, "q");
 				qfile = fopen(QNAME, "r");
 
 				if (! qfile) {
 					fclose(qfile);
 					cfree(line, LINESIZE);
 					exit(EXIT_FAILURE);
-				} else if (get_line(line, qfile)) {
-				} else {
+				} else if (! get_line(line, qfile)) {
+					fclose(qfile);
 					cfree(line, LINESIZE);
 					exit(EXIT_FAILURE);
+				} else {
+					fseek(qfile, s_len, SEEK_SET);
+					get_line(line, qfile);
+
+					check(strncmp(line, line3, LINESIZE) == 0);
+
+					fclose(qfile);
+					cfree(line, LINESIZE);
 				}
 			}
 		}
 
 		describe("del_item()") {
 			it("deletes the first line in the queuefile") {
-				check(strncmp(del_item(), "FIRST LINE\n", LINESIZE) == 0);
+				check(strncmp(del_item(), line1, LINESIZE) == 0);
 
 				qfile = fopen(QNAME, "r");
 
@@ -48,10 +63,11 @@ spec("Features") {
 					cfree(line, LINESIZE);
 					exit(EXIT_FAILURE);
 				} else if (! get_line(line, qfile)) {
+					fclose(qfile);
 					cfree(line, LINESIZE);
 					exit(EXIT_FAILURE);
 				} else {
-					check(strncmp(line, "SECOND LINE\n", LINESIZE) == 0);
+					check(strncmp(line, line2, LINESIZE) == 0);
 				}
 			}
 		}
