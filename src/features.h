1df73a4874035ee5a517d64575287307530d9f25
ren <code@apotheon.net>
2018-04-09 17:51:58 +0000

    pass remove-number numeric argument through to del_line()

--- src/features.h
+++ src/features.h
@@ -6,13 +6,12 @@
 #include "cli.h"
 #include "utils.h"
 
-char *del_item(unsigned int itemno);
+char *del_item(uint16_t itemno);
 
 void add_item(char *input, char *self);
 void del(char *self);
 void list_all(char *self);
 void print_numbered_file_listing(FILE *qfile);
-void remove_item_number(unsigned int itemno);
 void rot(char *self);
 void rot_item(char *self);
 void show_head(char *self);
