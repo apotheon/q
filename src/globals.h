807b4036cdddb44ad45c183d0671c9938c7813b2
ren <ren@noemail.net>
2018-04-11 14:48:59 +0000

    switch to stdbool.h for Boolean type definition
    FossilOrigin-Name: 60ae06af1aee26d5fb261b68169024cac9c3c968

--- src/globals.h
+++ src/globals.h
@@ -1,12 +1,11 @@
 #ifndef GLOBALS_H_
 #define GLOBALS_H_
 
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 #include "config.h"
-
-typedef enum { false, true } bool;
 
 #endif
