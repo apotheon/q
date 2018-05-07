fd82365b772573d91ddb1b19cb43269ad6d9be24
ren <code@apotheon.net>
2018-05-07 11:31:37 +0000

    reorganize q_spec.c a little

--- src/q_spec.c
+++ src/q_spec.c
@@ -4,24 +4,26 @@
 #include "bdd-for-c.h"
 
 spec("queuer") {
+	char *home = getenv("HOME");
+
 	char *qpath = calloc(LINESIZE, sizeof(*qpath));
 	check_alloc(qpath);
-	snprintf(qpath, LINESIZE, "tmp/%s/%s", DIRNAME, QNAME);
+	snprintf(qpath, LINESIZE, "%s/%s/%s", home, DIRNAME, QNAME);
+
+	char *output = calloc(LINESIZE, sizeof(*output));
+	check_alloc(output);
 
 	after_each() {
 		cleanup_testq();
+		cd("..");
 	}
 
 	describe("create-fresh-queue") {
 		it("should create a fresh queue file") {
-			uint32_t count;
 			FILE *listing;
 
 			char *ls = calloc(LINESIZE, sizeof(*ls));
 			check_alloc(ls);
-
-			char *output = calloc(LINESIZE, sizeof(*output));
-			check_alloc(output);
 
 			snprintf(ls, LINESIZE, "ls %s 2>/dev/null", qpath);
 
